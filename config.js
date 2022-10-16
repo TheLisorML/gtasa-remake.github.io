264fa4d154d98cf67a5b15553ffeabae7220cc84
TheLisorML <92590762+TheLisorML@users.noreply.github.com>
2022-10-16 21:24:42 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "GTASA Remake 2.0";
+var server_ip = "57.128.136.143";
+var server_port = 30083;
+var ip_copied = "IP copied!";
+
+var server_ip_be = "57.128.136.143";
+var server_port_be = 30083;
+var ip_copied_be = "IP copied!";
+
+var firefly_count = 20;
